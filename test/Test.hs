079c83b282e29e537a02d5855909a0ea84cbfc93
Roy Levien <royl@aldaron.com>
2015-10-04 18:19:29 -0400

    Add testing for historical Enigma messages

--- test/Test.hs
+++ test/Test.hs
@@ -5,6 +5,11 @@ import System.Exit
 import Crypto.Enigma
 
 
+-- See: http://www.enigma.hoerenberg.com/index.php?cat=The%20U534%20messages
+testHistoricalMessage :: String -> EnigmaConfig -> Message -> String -> Test
+testHistoricalMessage n cfg msg enc = TestCase $ assertEqual ("Error processing historical message " ++ n)
+        enc
+        (enigmaEncoding cfg msg)
 
 testRotorNames :: Test
 testRotorNames = TestCase $ assertEqual "Invalid rotor list"
@@ -16,22 +21,37 @@ testReflectorNames = TestCase $ assertEqual "Invalid reflector list"
         ["A","B","C","b","c"]
         reflectors
 
+testWindowsInstantiation :: String -> String -> String -> String -> Test
+testWindowsInstantiation rots winds plug rings = TestCase $ assertEqual "Invalid windows from instantiation"
+        winds
+        (windows $ configEnigma rots winds plug rings)
+
+test1 :: Test
 test1 = TestCase $ assertEqual "Should be one" 1 1
 
 test2 :: Test
 test2 = TestCase $ assertEqual "Shold both be zero" 0 0
 
-test3 :: Test
-test3 = TestCase $ assertEqual "Should be same"
-        "XQVI"
-        (windows $ configEnigma "b-β-V-VIII-II" "XQVI" "UX.MO.KZ.AY.EF.PL" "03.17.24.11")
-
 
 main :: IO ()
 main = do
-        print rotors
-        print reflectors
-        results <- runTestTT $ TestList [testRotorNames, testReflectorNames, test1, test2, test3, test3]
+        -- print rotors
+        -- print reflectors
+        results <- runTestTT $ TestList [
+                testRotorNames,
+                testReflectorNames,
+                testWindowsInstantiation "b-β-V-VIII-II" "XQVI" "UX.MO.KZ.AY.EF.PL" "03.17.24.11",
+                testWindowsInstantiation "C-V-VIII-III" "MUM" "AY.EF.PL" "09.16.24",
+                testWindowsInstantiation "c-β-VIII-VII-IV" "LMOI" "" "21.01.19.01",
+                testHistoricalMessage "U534-P1030662"
+                        (configEnigma "c-β-V-VI-VIII" "WIIJ" "AE.BF.CM.DQ.HU.JN.LX.PR.SZ.VW" "01.01.05.12")
+                        "UUUVIRSIBENNULEINSYNACHRXUUUSTUETZPUNKTLUEBECKVVVCHEFVIERXUUUFLOTTXXMITUUUVIERSIBENNULZWOUNDUUUVIERSIBENNULDREIZURFLENDERWERFTLUEBECKGEHENXFONDORTFOLGTWEITERESX"
+                        "LIRZMLWRCDMSNKLKBEBHRMFQFEQAZWXBGBIEXJPYFCQAAWSEKDEACOHDZKCZTOVSYHFNSCMAIMIMMAVJNLFXEWNPUIRINOZNCRVDHCGKCYRVUJQPVKEUIVVXGLQMKRJMDMLXLLRLYBKJWRXBQRZWGCCNDOPMGCKJ",
+                 testHistoricalMessage "U534-P1030694"
+                         (configEnigma "b-γ-IV-III-VIII" "RCPO" "CH.EJ.NV.OU.TY.LG.SZ.PK.DI.QB" "01.01.03.21")
+                         "PLLEVONVONZEHNXSIDIXXHAFENWARNEMUENDEFEINDBESETZTCHV"
+                         "VBBHSYTWZEEDGKYCAKYVWBWUUZVZIGCTBZLZYUHYWILFYUPBIPCM",
+               test1, test2]
         if (errors results + failures results == 0) then
                 exitWith ExitSuccess
         else
